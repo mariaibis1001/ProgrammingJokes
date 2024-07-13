46b36920ec587d595745cbc68602cc45358e0766
Maria Fernandez <mariaibis@csu.fullerton.edu>
2024-07-13 18:48:12 -0400

    Set up API call and add css.

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+        res.render("index.ejs", {joke: "..."});
 });
+
+app.get("/joke", async (req, res) => {
+    try {
+        const result = await axios.get("https://v2.jokeapi.dev/joke/Programming");
+        res.render("index.ejs", {joke: result.data.joke});
+    }
+    catch(error) {
+        console.error("Error with the api request.");
+    }
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
